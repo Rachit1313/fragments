5272929794e4f86b380ddbf294c674761ebc4420
Rachit1313 <Rachitchawla33@gmail.com>
2023-09-17 20:38:45 -0400

    Set up passport and authenticate to use AWS

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 // src/routes/index.js
 
 const express = require('express');
-
+const { authenticate } = require('../auth');
 // version and author from package.json
 const { version, author } = require('../../package.json');
 
 // Create a router that we can use to mount our API
 const router = express.Router();
-
+router.use(`/v1`, authenticate(), require('./api'));
 /**
  * Expose all of our API routes on /v1/* to include an API version.
  */

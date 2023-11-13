ef74d15a985e16598676e192e38f2104e21239f6
Rachit1313 <Rachitchawla33@gmail.com>
2023-11-13 16:23:43 -0500

    GET /fragments/:id returns an existing fragment's data with the expected Content-Type, with unit tests.

--- src/routes/api/get-by-id.js
+++ src/routes/api/get-by-id.js
@@ -1,11 +1,21 @@
+const logger = require('../../logger');
 const { Fragment } = require('../../model/fragment');
 const { createErrorResponse } = require('../../response');
 
 module.exports = async (req, res) => {
-  const fragment = await Fragment.byId(req.user, req.params.id);
-  const data = await fragment.getData();
-  if (!fragment) {
-    res.status(204).json(createErrorResponse(404, 'No fragment found with this id'));
+  try {
+    logger.info('request inside get by ID route');
+    const fragment = await Fragment.byId(req.user, req.params.id);
+    const data = await fragment.getData();
+    logger.info('retrieved fragment data');
+    logger.info(fragment);
+    if (!fragment) {
+      res.status(204).json(createErrorResponse(404, 'No fragment found with this id'));
+    }
+    logger.debug('fragment type : ' + fragment.type);
+    res.set('Content-Type', fragment.type);
+    res.status(200).send(data);
+  } catch (err) {
+    res.status(404).json(createErrorResponse(404, `Unknown Fragment`));
   }
-  res.status(200).json(data.toString());
 };
--- tests/unit/get-by-id.test.js
+++ tests/unit/get-by-id.test.js
@@ -5,6 +5,15 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('GET /v1/fragments/:id', () => {
+  test('unauthenticated requests are denied', () =>
+    request(app).get('/v1/fragments/randomid').expect(401));
+
+  test('incorrect credentials are denied', () =>
+    request(app)
+      .get('/v1/fragments/randomid')
+      .auth('invalid@email.com', 'incorrect_password')
+      .expect(401));
+
   test('authenticated users get a the fragment as expected', async () => {
     const postRes = await request(app)
       .post('/v1/fragments')
@@ -16,6 +25,14 @@ describe('GET /v1/fragments/:id', () => {
       .get(`/v1/fragments/${fragmentId}`)
       .auth('user1@email.com', 'password1');
     expect(getRes.statusCode).toBe(200);
-    expect(getRes.body).toBe('new fragment');
+    expect(getRes.text).toBe('new fragment');
+    expect(getRes.headers['content-type']).toBe('text/plain; charset=utf-8');
+  });
+
+  test('authenticated users get error if fragment does not exist', async () => {
+    const getRes = await request(app)
+      .get(`/v1/fragments/invalidID`)
+      .auth('user1@email.com', 'password1');
+    expect(getRes.statusCode).toBe(404);
   });
 });
